c2fd14f204f77691e0f17a4fcc269fb105048fae
elvadi1425 <elizavetapogosova08@gmail.com>
2026-06-08 03:47:36 +0500

    Update AdminLoginController.java

--- NailStudio/admin/src/main/java/com/nailstudio/admin/AdminLoginController.java
+++ NailStudio/admin/src/main/java/com/nailstudio/admin/AdminLoginController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
+import javafx.scene.control.Alert;ЫФ
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
