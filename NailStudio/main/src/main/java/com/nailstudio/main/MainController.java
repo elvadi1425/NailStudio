975bfd380238d577ab49cd15a1733a27b5d56f31
elvadi1425 <elizavetapogosova08@gmail.com>
2026-06-09 22:16:08 +0500

    Добавлен логотип компании

--- NailStudio/main/src/main/java/com/nailstudio/main/MainController.java
+++ NailStudio/main/src/main/java/com/nailstudio/main/MainController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
+import java.io.IOException; ава
 
 public class MainController {
 
