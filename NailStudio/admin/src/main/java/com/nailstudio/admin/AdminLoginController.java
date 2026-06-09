807dcc05e56b5e812c8c07f344dc8a2f22bf53df
elvadi1425 <elizavetapogosova08@gmail.com>
2026-06-09 22:07:15 +0500

    Добавлена кнопка входа

--- NailStudio/admin/src/main/java/com/nailstudio/admin/AdminLoginController.java
+++ NailStudio/admin/src/main/java/com/nailstudio/admin/AdminLoginController.java
@@ -2,7 +2,7 @@ package com.nailstudio.admin;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXMLLoader; ав
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;ЫФ
